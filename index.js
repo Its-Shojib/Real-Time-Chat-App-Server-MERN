01754cf4971cac55a98852340f1f7cbefb5b8c7d
Md Shojib Hossain <mdshojib922@gmail.com>
2024-03-04 19:33:22 +0600

    Express Server Installed

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Chat app is running!');
+})
+
+app.listen(port, () => {
+  console.log(`Real time chat app listening on ${port}`);
+})
