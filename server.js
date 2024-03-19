51bcbf323507bc04ccf7b82fc66fb0b5c74fb027
Md Shojib Hossain <mdshojib922@gmail.com>
2024-03-19 17:49:10 +0600

    Server Updated

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 app.get("/", (req, res) => {
 	res.send('Server is Running');
 });
